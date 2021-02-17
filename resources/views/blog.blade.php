7aaf7aa268700f1b8f9eeda0812a4ea1f854266c
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-17 16:45:38 +0100

    Stampati in blog i posts presi dal database
    Stampati in blog i posts presi dal database

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.app')
+@section('posts')
+    <h1>Posts</h1>
+    <div>
+        @foreach ($posts as $post) 
+            <h3>{{$post->title}}</h3>
+            <p>{{$post->body}}</p>
+        @endforeach
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 @include('layouts.head')
     <body>
         @include('layouts.header')
+        @yield('posts')
     </body>
 </html>
